0693e092e266a574c7a95ea3de9ab199bbdd4f54
qsmcgogo <463803609@qq.com>
2020-11-11 14:29:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/game2048/GameActivity.java
@@ -0,0 +1,23 @@
+package com.example.game2048;
+
+import android.app.Activity;
+import android.os.Bundle;
+/*
+author:smc
+date 2020/11/11 14:26
+ */
+public class GameActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/game2048/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.game2048;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+public class MainActivity extends Activity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+}
